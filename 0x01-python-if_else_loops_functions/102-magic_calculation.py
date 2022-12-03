cad8151fa6b82ef6aca0dc7302bd032765275d98
Judith Oguche <judithoguche@gmail.com>
2022-12-03 10:31:42 +0100

    function def magic_calculation(a, b, c): that does exactly the same as the following Python bytecode

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,1 +1,10 @@
-dnjdn
+#!/usr/bin/python3
+# Author -Oguche Judith
+
+def magic_calculation(a, b, c):
+    """Match bytecode provided by Holberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
