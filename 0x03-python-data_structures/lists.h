2466de6c0d40a6a3d72ac2de2705a86a256a2d5a
ojoago1 <judithoguche@gmail.com>
2022-12-05 03:42:56 -0800

    lists

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -25,5 +25,5 @@ void free_listint(listint_t *head);
 
 void reverse_listint(listint_t **head);
 int is_palindrome(listint_t **head);
-
+void print_python_list_info(PyObject *p);
 #endif /* LISTS_H */
